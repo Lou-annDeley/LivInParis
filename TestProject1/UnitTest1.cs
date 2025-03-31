e6278ffd2fbbf7140a921bc70a215defc0a0e95a
alixdrouin <alix.drouin@edu.devinci.fr>
2025-03-31 21:58:51 +0200

    commit
    test uniatire

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,11 +1,22 @@
+using DocumentFormat.OpenXml.Bibliography;
+using LivInParis___DELPIERRE_DROUIN_DELEY;
+using NuGet.Frameworks;
+
 namespace TestProject1
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Testdjikstra()
         {
+            Graphe<string> metro = new Graphe<string>();
+            string station = "Chatelet";
+            string station2 = "Liège";
+            Noeud<string> noeud = metro.AjouterSommet(station);
+            List<Noeud<string>> plus_petit_cheminS2 = metro.Dijkstra2(metro.Sommets[0], metro.Sommets[1]);
+            Assert.Equals(noeud, plus_petit_cheminS2);
+
         }
     }
 }

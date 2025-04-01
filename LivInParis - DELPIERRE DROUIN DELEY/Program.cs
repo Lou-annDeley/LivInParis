4a869a7e20fb0626a31a84f0e83206a4a0dbc284
alixdrouin <alix.drouin@edu.devinci.fr>
2025-04-01 14:36:06 +0200

    commit
    graphe

--- LivInParis - DELPIERRE DROUIN DELEY/Program.cs
+++ LivInParis - DELPIERRE DROUIN DELEY/Program.cs
@@ -14,19 +14,19 @@ namespace LivInParis___DELPIERRE_DROUIN_DELEY
         static void Main(string[] args)
         {
 
-            //MySqlConnection maConnexion = null;
-            //try
-            //{
-            //    string connexionString = "SERVER=localhost; PORT=3306;" + "DATABASE=Livraison;" + "UID=root;PASSWORD=root";
-            //    maConnexion = new MySqlConnection(connexionString);
-            //    maConnexion.Open();
-                
-            //}
-            //catch(MySqlException e )
-            //{
-            //    Console.WriteLine("ErreurConnexion :"+e.ToString());
-            //    return;
-            //}
+            MySqlConnection maConnexion = null;
+            try
+            {
+                string connexionString = "SERVER=localhost; PORT=3306;" + "DATABASE=Livraison;" + "UID=root;PASSWORD=root";
+                maConnexion = new MySqlConnection(connexionString);
+                maConnexion.Open();
+
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine("ErreurConnexion :" + e.ToString());
+                return;
+            }
 
 
 
